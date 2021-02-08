9f00627be6a6ff5474f96bf03004577f13c3a90b
Sergey Yelenski <sergey.yelenski@warecorp.com>
2021-02-08 15:15:08 +0300

    migration changed

--- db/migrate/20090508035844_add_default_assignee_to_project.rb
+++ db/migrate/20090508035844_add_default_assignee_to_project.rb
@@ -1,4 +1,4 @@
-class AddDefaultAssigneeToProject < ActiveRecord::Migration
+class AddDefaultAssigneeToProject < ActiveRecord::Migration[5.2]
   def self.up
     add_column :projects, :default_assignee_id, :integer
   end
